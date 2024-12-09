6dbe188ca5f9d3518268977945a5e2abb53f24df
Haolin Dai <haolin.dai@workday.com>
2024-12-09 00:35:15 -0800

    KONG-3 add controller and unit test

--- cmd/controller_test.go
+++ cmd/controller_test.go
@@ -9,13 +9,9 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+// Load Test Data
+
 func TestGetAllServices(t *testing.T) {
-
-	services = []ServiceResponse{
-		{ID: "1", Name: "Service1"},
-		{ID: "2", Name: "Service2"},
-	}
-
 	req, err := http.NewRequest("GET", "/services", nil)
 	assert.NoError(t, err)
 
@@ -24,35 +20,13 @@ func TestGetAllServices(t *testing.T) {
 	handler.ServeHTTP(rr, req)
 
 	assert.Equal(t, http.StatusOK, rr.Code)
-	expected := `[{"id":"1","name":"Service1"},{"id":"2","name":"Service2"}]`
-	assert.JSONEq(t, expected, rr.Body.String())
-}
-
-func TestGetServiceById(t *testing.T) {
-	services = []ServiceResponse{
-		{ID: "1", Name: "Service1"},
-		{ID: "2", Name: "Service2"},
+	expected_service_names := []string{"Service 1", "Service 2"}
+	// the response body should contain the expected service names
+	for _, name := range expected_service_names {
+		assert.Contains(t, rr.Body.String(), name)
 	}
-
-	req, err := http.NewRequest("GET", "/services/1", nil)
-	assert.NoError(t, err)
-
-	rr := httptest.NewRecorder()
-	router := mux.NewRouter()
-	router.HandleFunc("/services/{id}", GetServiceById)
-	router.ServeHTTP(rr, req)
-
-	assert.Equal(t, http.StatusOK, rr.Code)
-	expected := `{"id":"1","name":"Service1"}`
-	assert.JSONEq(t, expected, rr.Body.String())
 }
-
 func TestGetAllUsers(t *testing.T) {
-	users = []UserResponse{
-		{ID: "1", Name: "User1"},
-		{ID: "2", Name: "User2"},
-	}
-
 	req, err := http.NewRequest("GET", "/users", nil)
 	assert.NoError(t, err)
 
@@ -61,35 +35,13 @@ func TestGetAllUsers(t *testing.T) {
 	handler.ServeHTTP(rr, req)
 
 	assert.Equal(t, http.StatusOK, rr.Code)
-	expected := `[{"id":"1","name":"User1"},{"id":"2","name":"User2"}]`
-	assert.JSONEq(t, expected, rr.Body.String())
-}
-
-func TestGetUserById(t *testing.T) {
-	users = []UserResponse{
-		{ID: "1", Name: "User1"},
-		{ID: "2", Name: "User2"},
+	expected_usernames := []string{"user1", "user2"}
+	// the response body should contain the expected usernames
+	for _, name := range expected_usernames {
+		assert.Contains(t, rr.Body.String(), name)
 	}
-
-	req, err := http.NewRequest("GET", "/users/1", nil)
-	assert.NoError(t, err)
-
-	rr := httptest.NewRecorder()
-	router := mux.NewRouter()
-	router.HandleFunc("/users/{id}", GetUserById)
-	router.ServeHTTP(rr, req)
-
-	assert.Equal(t, http.StatusOK, rr.Code)
-	expected := `{"id":"1","name":"User1"}`
-	assert.JSONEq(t, expected, rr.Body.String())
 }
-
 func TestGetServiceByIdNotFound(t *testing.T) {
-	services = []ServiceResponse{
-		{ID: "1", Name: "Service1"},
-		{ID: "2", Name: "Service2"},
-	}
-
 	req, err := http.NewRequest("GET", "/services/3", nil)
 	assert.NoError(t, err)
 
@@ -102,11 +54,6 @@ func TestGetServiceByIdNotFound(t *testing.T) {
 }
 
 func TestGetUserByIdNotFound(t *testing.T) {
-	users = []UserResponse{
-		{ID: "1", Name: "User1"},
-		{ID: "2", Name: "User2"},
-	}
-
 	req, err := http.NewRequest("GET", "/users/3", nil)
 	assert.NoError(t, err)
 
@@ -116,87 +63,4 @@ func TestGetUserByIdNotFound(t *testing.T) {
 	router.ServeHTTP(rr, req)
 
 	assert.Equal(t, http.StatusNotFound, rr.Code)
-}
-
-// API tests
-func TestGetAllServicesAPI(t *testing.T) {
-	ts := httptest.NewServer(http.HandlerFunc(GetAllServices))
-	defer ts.Close()
-
-	res, err := http.Get(ts.URL)
-	assert.NoError(t, err)
-	assert.Equal(t, http.StatusOK, res.StatusCode)
-}
-
-func TestGetServiceByIdAPI(t *testing.T) {
-	services = []ServiceResponse{
-		{ID: "1", Name: "Service1"},
-		{ID: "2", Name: "Service2"},
-	}
-
-	router := mux.NewRouter()
-	router.HandleFunc("/services/{id}", GetServiceById)
-	ts := httptest.NewServer(router)
-	defer ts.Close()
-
-	res, err := http.Get(ts.URL + "/services/1")
-	assert.NoError(t, err)
-	assert.Equal(t, http.StatusOK, res.StatusCode)
-}
-
-func TestGetAllUsersAPI(t *testing.T) {
-	ts := httptest.NewServer(http.HandlerFunc(GetAllUsers))
-	defer ts.Close()
-
-	res, err := http.Get(ts.URL)
-	assert.NoError(t, err)
-	assert.Equal(t, http.StatusOK, res.StatusCode)
-}
-
-func TestGetUserByIdAPI(t *testing.T) {
-	users = []UserResponse{
-		{ID: "1", Name: "User1"},
-		{ID: "2", Name: "User2"},
-	}
-
-	router := mux.NewRouter()
-	router.HandleFunc("/users/{id}", GetUserById)
-	ts := httptest.NewServer(router)
-	defer ts.Close()
-
-	res, err := http.Get(ts.URL + "/users/1")
-	assert.NoError(t, err)
-	assert.Equal(t, http.StatusOK, res.StatusCode)
-}
-
-func TestGetServiceByIdNotFoundAPI(t *testing.T) {
-	services = []ServiceResponse{
-		{ID: "1", Name: "Service1"},
-		{ID: "2", Name: "Service2"},
-	}
-
-	router := mux.NewRouter()
-	router.HandleFunc("/services/{id}", GetServiceById)
-	ts := httptest.NewServer(router)
-	defer ts.Close()
-
-	res, err := http.Get(ts.URL + "/services/3")
-	assert.NoError(t, err)
-	assert.Equal(t, http.StatusNotFound, res.StatusCode)
-}
-
-func TestGetUserByIdNotFoundAPI(t *testing.T) {
-	users = []UserResponse{
-		{ID: "1", Name: "User1"},
-		{ID: "2", Name: "User2"},
-	}
-
-	router := mux.NewRouter()
-	router.HandleFunc("/users/{id}", GetUserById)
-	ts := httptest.NewServer(router)
-	defer ts.Close()
-
-	res, err := http.Get(ts.URL + "/users/3")
-	assert.NoError(t, err)
-	assert.Equal(t, http.StatusNotFound, res.StatusCode)
 }
--- cmd/main.go
+++ cmd/main.go
@@ -4,14 +4,8 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"os"
 
-	"github.com/golang-migrate/migrate/v4"
-	"github.com/golang-migrate/migrate/v4/database/postgres"
 	_ "github.com/golang-migrate/migrate/v4/source/file"
-	gormPostgres "gorm.io/driver/postgres"
-	"gorm.io/gorm"
-	"gorm.io/gorm/logger"
 
 	"github.com/gorilla/mux"
 )
@@ -20,104 +14,12 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello, World!")
 }
 
-func getDsn() string {
-	dbHost := os.Getenv("SERVICE_DASHBOARD_DB_HOST")
-	if dbHost == "" {
-		if os.Getenv("UNIT_TEST") == "True" {
-			dbHost = "localhost"
-		} else {
-			log.Println("Warning: SERVICE_DASHBOARD_DB_HOST is not set. Using default value 'host.docker.internal'.")
-			dbHost = "host.docker.internal"
-		}
-	}
-	dbPort := os.Getenv("SERVICE_DASHBOARD_DB_PORT")
-	if dbPort == "" {
-		dbPort = "5432"
-	}
-	dbUser := os.Getenv("SERVICE_DASHBOARD_DB_USER")
-	if dbUser == "" {
-		dbUser = "postgres"
-	}
-	dbPassword := os.Getenv("SERVICE_DASHBOARD_DB_PASSWORD")
-	if dbPassword == "" {
-		dbPassword = "example"
-	}
-	dbName := os.Getenv("SERVICE_DASHBOARD_DB_NAME")
-	if dbName == "" {
-		dbName = "postgres"
-	}
-
-	dsn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
-		dbHost, dbPort, dbUser, dbPassword, dbName)
-
-	return dsn
-}
-
-func getGormDB() (*gorm.DB, error) {
-	db, err := gorm.Open(gormPostgres.Open(getDsn()), &gorm.Config{
-		Logger: logger.Default.LogMode(logger.Info),
-	})
-
-	if err != nil {
-		return nil, err
-	}
-
-	return db, nil
-}
-
-func initDB() (*gorm.DB, error) {
-	db, err := getGormDB()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	sqlDB, err := db.DB()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	driver, err := postgres.WithInstance(sqlDB, &postgres.Config{})
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// Auto migrate the schema
-	db.AutoMigrate(&Service{}, &ServiceVersion{}, &User{}, &UserProfile{})
-
-	// Run migrations from the migrations folder
-	migrationsPath := "file://./migrations" // Path to your migrations folder
-	m, err := migrate.NewWithDatabaseInstance(
-		migrationsPath,
-		"postgres", driver,
-	)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	if err := m.Up(); err != nil && err != migrate.ErrNoChange && err != migrate.ErrNilVersion {
-		log.Fatal(err)
-	}
-
-	// Inject DummyData only if the table is empty
-	GenerateDummyData(db)
-
-	return db, nil
-}
-
 func main() {
-	_, err := initDB()
-	if err != nil {
-		fmt.Println(err)
-		panic("failed to initialize database")
-	}
-
-	if err := http.ListenAndServe(":8080", nil); err != nil {
-		fmt.Println("Error starting server:", err)
-	}
+	InitDB()
 
 	router := mux.NewRouter()
 	router.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "ok")
+		log.Println("ok")
 	})
 	router.HandleFunc("/v1/services", GetAllServices).Methods("GET")
 	router.HandleFunc("/v1/services/{id}", GetServiceById).Methods("GET")
@@ -127,5 +29,8 @@ func main() {
 	// mount the router on the server
 	http.Handle("/", router)
 
-	fmt.Println("Starting server on :8080")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		fmt.Println("Error starting server:", err)
+	}
+	log.Println("Starting server on :8080")
 }
